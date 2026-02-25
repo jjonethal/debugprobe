73bdfb4bbbb41367f272e85ee1ea5d7468f0d93a
Jean Jonethal <jean.jonethal@gigaset.com>
2026-02-25 11:14:42 +0100

    update pico_sdk path

--- build-pico2w.sh
+++ build-pico2w.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PICO_SDK_PATH=$(realpath ../pico-sdk)
+export PICO_SDK_PATH=$(realpath ../pico_sdk)
 
 cmake -B build-pico2 -DDEBUG_ON_PICO=1 -DPICO_BOARD=pico2_w -DPICO_PLATFORM=rp2350
 cmake --build build-pico2
